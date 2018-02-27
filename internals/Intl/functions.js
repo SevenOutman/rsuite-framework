17b57bc976edbfa28f9a109f5e60447025da4fc2
Doma <leishenghao@126.com>
2018-02-27 16:08:53 +0800

    localize() uses id as fallback

--- internals/Intl/functions.js
+++ internals/Intl/functions.js
@@ -1,6 +1,6 @@
 import { app } from '../App/functions';
 
 export function localize(id) {
-  return app('intl').messages()[id];
+  return app('intl').messages()[id] || id /* if none, fallback */;
 }
 
